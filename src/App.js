9a29c57ed999a06f84520a9d936dc8edb8c87df6
Giorgi surmanidze <octopusoctopus796@gmail.com>
2022-06-26 20:59:43 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,66 +1,146 @@
-import React, { useEffect, useState } from "react";
-import NotesList from "./components/NotesList";
-import { nanoid } from "nanoid";
-import Search from "./Search";
-import Header from "./components/Header";
+import { useState, useEffect } from "react";
+import Square from "./components/Square";
+import { Patterns } from "./components/Patterns";
 
-const App = () => {
-  const [searchText, setSearchText] = useState("");
-
-  const [notes, setNotes] = useState([
-    {
-      id: nanoid(),
-      text: "Make Note...",
-      date: "06/26/2022",
-    },
-  ]);
-  const addNote = (text) => {
-    const date = new Date();
-    const newNote = {
-      id: nanoid(),
-      text: text,
-      date: date.toLocaleDateString(),
-    };
-    const newNotes = [...notes, newNote];
-    setNotes(newNotes);
-  };
-
-  const deleteNote = (id) => {
-    const newNotes = notes.filter((note) => note.id !== id);
-    setNotes(newNotes);
-  };
-
-  const [darkMode, setDarkMode] = useState(false);
+function App() {
+  const [board, setBoard] = useState(Array(9).fill(""));
+  const [player, setPlayer] = useState("O");
+  const [result, setResult] = useState({ winner: "none", state: "none" });
 
   useEffect(() => {
-    const savedNotes = JSON.parse(localStorage.getItem("Note-App"));
+    checkWin();
+    checkIfTie();
 
-    if (savedNotes) {
-      setNotes(savedNotes);
+    if (player == "X") {
+      setPlayer("O");
+    } else {
+      setPlayer("X");
     }
-  }, []);
+  }, [board]);
 
   useEffect(() => {
-    localStorage.setItem("Note-App", JSON.stringify(notes));
-  }, [notes]);
+    if (result.state != "none") {
+      alert(`Game Finished! Winning Player: ${result.winner}`);
+      restartGame();
+    }
+  }, [result]);
+
+  const chooseSquare = (square) => {
+    setBoard(
+      board.map((val, idx) => {
+        if (idx == square && val == "") {
+          return player;
+        }
+
+        return val;
+      })
+    );
+  };
+
+  const checkWin = () => {
+    Patterns.forEach((currPattern) => {
+      const firstPlayer = board[currPattern[0]];
+      if (firstPlayer == "") return;
+      let foundWinningPattern = true;
+      currPattern.forEach((idx) => {
+        if (board[idx] != firstPlayer) {
+          foundWinningPattern = false;
+        }
+      });
+
+      if (foundWinningPattern) {
+        setResult({ winner: player, state: "Won" });
+      }
+    });
+  };
+
+  const checkIfTie = () => {
+    let filled = true;
+    board.forEach((square) => {
+      if (square == "") {
+        filled = false;
+      }
+    });
+
+    if (filled) {
+      setResult({ winner: "No One", state: "Tie" });
+    }
+  };
+
+  const restartGame = () => {
+    setBoard(Array(9).fill(""));
+    setPlayer("O");
+  };
 
   return (
-    <div className={`${darkMode && "dark-mode"}`}>
-      <div className="containar">
-        <Header handleToggleDarkMode={setDarkMode} />
-        <Search handleSearchNote={setSearchText} />
-        <NotesList
-          notes={notes.filter((note) =>
-            note.text
-              .toLocaleLowerCase()
-              .includes(searchText.toLocaleLowerCase())
-          )}
-          handleAddNote={addNote}
-          deleteNote={deleteNote}
-        />
+    <div>
+      <h1> TIC TAC TOE </h1>
+    <div className="App">
+      <div className="board">
+        <div className="row">
+          <Square
+            val={board[0]}
+            chooseSquare={() => {
+              chooseSquare(0);
+            }}
+          />
+          <Square
+            val={board[1]}
+            chooseSquare={() => {
+              chooseSquare(1);
+            }}
+          />
+          <Square
+            val={board[2]}
+            chooseSquare={() => {
+              chooseSquare(2);
+            }}
+          />
+        </div>
+        <div className="row">
+          <Square
+            val={board[3]}
+            chooseSquare={() => {
+              chooseSquare(3);
+            }}
+          />
+          <Square
+            val={board[4]}
+            chooseSquare={() => {
+              chooseSquare(4);
+            }}
+          />
+          <Square
+            val={board[5]}
+            chooseSquare={() => {
+              chooseSquare(5);
+            }}
+          />
+        </div>
+        <div className="row">
+          <Square
+            val={board[6]}
+            chooseSquare={() => {
+              chooseSquare(6);
+            }}
+          />
+          <Square
+            val={board[7]}
+            chooseSquare={() => {
+              chooseSquare(7);
+            }}
+          />
+          <Square
+            val={board[8]}
+            chooseSquare={() => {
+              chooseSquare(8);
+            }}
+          />
+        </div>
       </div>
     </div>
+    </div>
   );
-};
+}
 
 export default App;
